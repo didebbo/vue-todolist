3ba2e795bd0c50d9b10d98af2b5bda320ed40a8b
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-20 16:07:22 +0200

    aggiunta funzione addToDo

--- js/root.js
+++ js/root.js
@@ -2,9 +2,13 @@ const root = new Vue(
     {
         el: "#root",
         data: {
+            toDo: "",
             toDos: ["Andare a Scuola", "Fare i compiti", "Andare in palestra"]
         },
         methods: {
+            addToDo: function () {
+                if (this.toDo != "") this.toDos.push(this.toDo);
+            },
             deleteToDo: function (index) {
                 const tmpToDos = [];
                 this.toDos.forEach((toDo, i) => {
